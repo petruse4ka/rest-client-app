31e84dee527b3b60ca60527963013b5125118f3f
tearzday <biverdaniil@gmail.com>
2025-09-04 17:02:38 +0300

    feat: add page links in header

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,11 +1,13 @@
 import { ThemeContext } from '@/context/theme-context';
 import { MoonOutlined, SunOutlined } from '@ant-design/icons';
-import { Button, Flex, Select, Switch } from 'antd';
+import { Button, Flex, Row, Select, Switch } from 'antd';
 import { Header } from 'antd/es/layout/layout';
-import { ReactNode, useContext } from 'react';
+import Link from 'next/link';
+import { ReactNode, useContext, useState } from 'react';
 
 export function HeaderApp() {
   const { themeValue, setThemeValue } = useContext(ThemeContext);
+  const [isLogin, setIsLogin] = useState<boolean>(true);
 
   const handleThemeChange = (checked: boolean) => {
     setThemeValue(checked ? 'dark' : 'light');
@@ -16,30 +18,52 @@ export function HeaderApp() {
   };
 
   return (
-    <Header>
-      <Flex justify="space-between" align="center">
-        <img src="./logo.png" alt="Logo company" className="w-10" />
-        <Switch
-          checked={themeValue === 'dark'}
-          onChange={handleThemeChange}
-          checkedChildren={<MoonOutlined />}
-          unCheckedChildren={<SunOutlined />}
-        />
-        <Select
-          defaultValue={{ value: 'ru', label: 'Russian' }}
-          style={{ width: 120 }}
-          onChange={handleChangeLanguage}
-          options={[
-            { value: 'ru', label: 'Russian' },
-            { value: 'en', label: 'English' },
-            { value: 'be', label: 'Belarusian' },
-          ]}
-        />
-        <div>
-          <Button type="primary">Sign In</Button>
-          <Button type="primary">Sign Up</Button>
-        </div>
-      </Flex>
-    </Header>
+    <>
+      <Header>
+        <Flex justify="space-between" gap="middle" align="center" className="h-full">
+          <Flex align="center" gap="middle">
+            <Switch
+              checked={themeValue === 'dark'}
+              onChange={handleThemeChange}
+              checkedChildren={<MoonOutlined />}
+              unCheckedChildren={<SunOutlined />}
+            />
+            <Select
+              defaultValue={{ value: 'ru', label: 'Russian' }}
+              style={{ width: 120 }}
+              onChange={handleChangeLanguage}
+              options={[
+                { value: 'ru', label: 'Russian' },
+                { value: 'en', label: 'English' },
+                { value: 'be', label: 'Belarusian' },
+              ]}
+            />
+          </Flex>
+
+          <Flex gap="middle" align="center">
+            <Link href="/">
+              <img src="./logo.png" alt="Logo company" className="w-10" />
+            </Link>
+            {isLogin && (
+              <Flex gap="middle">
+                <Link href="/">Home</Link>
+                <Link href="/">REST Client</Link>
+                <Link href="/">History</Link>
+                <Link href="/">Variables</Link>
+              </Flex>
+            )}
+          </Flex>
+
+          {isLogin ? (
+            <Button type="primary">Logout</Button>
+          ) : (
+            <Flex gap="middle">
+              <Button type="primary">Sign In</Button>
+              <Button type="primary">Sign Up</Button>
+            </Flex>
+          )}
+        </Flex>
+      </Header>
+    </>
   );
 }
