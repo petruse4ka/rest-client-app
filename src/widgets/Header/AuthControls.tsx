4de9429d03de415b844bfedebd12cda07340fe6f
tearzday <biverdaniil@gmail.com>
2025-09-06 01:48:59 +0300

    feat: add translate for header

--- src/widgets/Header/AuthControls.tsx
+++ src/widgets/Header/AuthControls.tsx
@@ -1,14 +1,16 @@
 import { Button, Flex } from 'antd';
+import { useTranslations } from 'next-intl';
 
 export default function AuthControls({ isLogin }: { isLogin: boolean }) {
+  const t = useTranslations('Header');
   return (
     <>
       {isLogin ? (
-        <Button type="primary">Sign Out</Button>
+        <Button type="primary">{t('signOut')}</Button>
       ) : (
         <Flex gap="middle">
-          <Button type="primary">Sign In</Button>
-          <Button type="primary">Sign Up</Button>
+          <Button type="primary">{t('signIn')}</Button>
+          <Button type="primary">{t('signUp')}</Button>
         </Flex>
       )}
     </>
