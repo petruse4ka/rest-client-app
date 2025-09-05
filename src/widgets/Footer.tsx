26a87b1cf4f32ccad8c7463c093e294247550d68
tearzday <biverdaniil@gmail.com>
2025-09-06 01:00:36 +0300

    refactor: errors found during the review

--- src/widgets/Footer.tsx
+++ src/widgets/Footer.tsx
@@ -1,23 +1,20 @@
 import { authorLinks } from '@/shared/config';
 import { GithubOutlined } from '@ant-design/icons';
 import { Divider, Flex, Grid, Space, Typography } from 'antd';
+import Image from 'next/image';
 import { Footer } from 'antd/es/layout/layout';
 const { useBreakpoint } = Grid;
 const { Text, Link } = Typography;
-
-const ImgStyle = {
-  width: '40px',
-};
 
 export function FooterApp() {
   const screens = useBreakpoint();
 
   return (
     <Footer>
-      <Flex justify="space-between" align="center" vertical={!screens.md}>
+      <Flex justify="space-between" align="center" vertical={!screens.md} gap={10}>
         <Space size="middle">
           <Link href="https://rs.school/courses/reactjs" target="_blank">
-            <img src="./rss-logo.svg" alt="RSS Logo" style={ImgStyle} />
+            <Image src="/rss-logo.svg" alt="RSS Logo" width={40} height={40} />
           </Link>
           <Text>© 2025</Text>
         </Space>
