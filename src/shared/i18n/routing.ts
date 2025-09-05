5561ead2b5ac03fd0be12b2b3539a4301daa2d7a
tearzday <biverdaniil@gmail.com>
2025-09-06 01:56:09 +0300

    feat: add change language

--- src/shared/i18n/routing.ts
+++ src/shared/i18n/routing.ts
@@ -1,6 +1,9 @@
 import { defineRouting } from 'next-intl/routing';
+import { languages } from '../config';
+
+const languagesKey = languages.map((language) => language.key);
 
 export const routing = defineRouting({
-  locales: ['en', 'ru'],
+  locales: languagesKey,
   defaultLocale: 'en',
 });
