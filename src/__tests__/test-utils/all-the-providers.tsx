375161362423ca22b6d5e9a4b3422f1eb1a2910b
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-09-06 22:31:41 +0200

    test: add theme provider to the test utils

--- src/__tests__/test-utils/all-the-providers.tsx
+++ src/__tests__/test-utils/all-the-providers.tsx
@@ -1,11 +1,12 @@
 import type { ReactNode } from 'react';
 import { NextIntlClientProvider } from 'next-intl';
+import { ThemeProvider } from '@/shared/provider';
 import enMessages from '@/shared/i18n/messages/en.json';
 
 export default function AllTheProviders({ children }: { children: ReactNode }) {
   return (
     <NextIntlClientProvider messages={enMessages} locale="en">
-      {children}
+      <ThemeProvider>{children}</ThemeProvider>
     </NextIntlClientProvider>
   );
 }
