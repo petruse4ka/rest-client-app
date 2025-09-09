91ed6a24e52c82e8c38448361466cf051e722d1c
tearzday <biverdaniil@gmail.com>
2025-09-09 16:13:10 +0300

    feat: add info for team

--- src/shared/UI/person-card.tsx
+++ src/shared/UI/person-card.tsx
@@ -22,13 +22,13 @@ export function PersonCard({ person }: PersonCardProps) {
     <Card
       title={
         <Space align="center" size="large">
-          <Avatar size="large" icon={<UserOutlined />} />
+          <Avatar size={50} src={img} alt={name} />
           <Title level={3}>{name}</Title>
         </Space>
       }
       style={{ width: '100%', maxWidth: '400px' }}
     >
-      <Flex vertical gap={15}>
+      <Flex justify="space-between" vertical gap={15} style={{ height: '100%' }}>
         <Text>{description}</Text>
         <Link href={github.link} target="_blank" style={{ alignSelf: 'flex-end' }}>
           <Space size="small">
