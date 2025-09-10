44ca13ea8542cb82e82465ac41c111dc47d4eb2d
tearzday <biverdaniil@gmail.com>
2025-09-10 15:30:06 +0300

    refactor: test for hero-section

--- src/widgets/__tests__/hero-section.test.tsx
+++ src/widgets/__tests__/hero-section.test.tsx
@@ -2,6 +2,21 @@ import { render } from '@/__tests__/test-utils/test-utils';
 import { screen } from '@testing-library/react';
 import { HeroSection } from '../hero-section';
 
+vi.mock('firebase/auth', () => ({
+  onAuthStateChanged: vi.fn((auth, callback) => {
+    callback(null);
+    return () => {};
+  }),
+}));
+
+vi.mock('@/shared/config/firebase', () => {
+  return {
+    auth: {
+      currentUser: null,
+    },
+  };
+});
+
 describe('Test for Hero Section', () => {
   test('Render', () => {
     render(<HeroSection />);
