f805a3837440856746ecb410c3f9d1c0a22d18b9
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-09-13 22:15:50 +0300

    chore: change session time to LONG

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -3,15 +3,15 @@ import { NextResponse } from 'next/server';
 import { adminAuth } from '@/server/firebase-admin';
 import { appRoutes } from '@/shared/config/navigation';
 import {
-  //SESSION_LONG_TTL,
-  SESSION_SHORT_TTL,
+  SESSION_LONG_TTL,
+  //SESSION_SHORT_TTL,
 } from '@/shared/config/auth';
 
 export async function POST(req: Request) {
   const { idToken } = await req.json();
 
-  //const expiresIn = SESSION_LONG_TTL;
-  const expiresIn = SESSION_SHORT_TTL;
+  const expiresIn = SESSION_LONG_TTL;
+  //const expiresIn = SESSION_SHORT_TTL;
 
   try {
     const sessionCookie = await adminAuth.createSessionCookie(idToken, { expiresIn });
