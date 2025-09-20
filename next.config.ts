09aceec448b3f422ac0380dda1aa72c2a1725bb9
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-09-20 16:55:32 +0200

    fix: add active button bg and fix the ant-compatibility error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    scrollRestoration: false,
+  },
 };
 
 const withNextIntl = createNextIntlPlugin('./src/shared/i18n/request.ts');
--- src/shared/UI/client-loader.tsx
+++ src/shared/UI/client-loader.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import '@ant-design/v5-patch-for-react-19';
 import { Spin, Flex } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import { useEffect, useState } from 'react';
