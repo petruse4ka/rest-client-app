bcc224827a459a9d2c5d72a41b33420d5804ba11
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-09-20 21:16:58 +0200

    fix: fix tests

--- src/widgets/header/mobile-header.tsx
+++ src/widgets/header/mobile-header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { MenuOutlined } from '@ant-design/icons';
 import { Button, Drawer, Flex, Space } from 'antd';
 import InterfaceSettings from './interface-settings';
@@ -20,15 +20,15 @@ export default function MobileHeader({ user }: MobileHeaderProps) {
     setDrawer(true);
   };
 
-  const hiddenDrawer = () => {
+  const hiddenDrawer = useCallback(() => {
     setDrawer(false);
-  };
+  }, []);
 
   const pathname = usePathname();
 
   useEffect(() => {
-    if (drawer) hiddenDrawer();
-  }, [pathname, drawer]);
+    setDrawer(false);
+  }, [pathname]);
 
   return (
     <>
