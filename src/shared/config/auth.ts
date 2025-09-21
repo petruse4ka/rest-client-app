9ec3497ebe78edabc276f9ebc5933addd4b7db47
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-09-21 15:45:06 +0200

    refactor: increase token time to live to 20 min

--- src/shared/config/auth.ts
+++ src/shared/config/auth.ts
@@ -1,5 +1,5 @@
 export const SESSION_LONG_TTL = 1000 * 60 * 60 * 24 * 7;
-export const SESSION_SHORT_TTL = 1000 * 60 * 5;
+export const SESSION_SHORT_TTL = 1000 * 60 * 20;
 export const SESSION_TTL = SESSION_SHORT_TTL;
 
 export const CHECK_TOKEN_INTERVAL = 1000 * 60;
